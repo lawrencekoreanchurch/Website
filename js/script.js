2a9057dc5ecc7970c3167ccac220322bad78fbbb
lawrencekoreanchurch <lawrencekoreanchurch@gmail.com>
2022-03-03 13:51:35 -0600

    First commit to Website depo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const checkpoint = 1000;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".main-overlay").style.opacity = opacity;
+});
