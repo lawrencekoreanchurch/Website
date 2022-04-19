bcc96bc15f776972fdfa31361d120a13caf85324
lawrencekoreanchurch <lawrencekoreanchurch@gmail.com>
2022-04-18 23:27:10 -0500

    FAQ section and more
    FAQ section accordion and more

--- js/script.js
+++ js/script.js
@@ -10,24 +10,3 @@ $(function () {
     }
   });
 });
-
-/* window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll >= 0) {
-    document.querySelector(".test").add("scroll");
-  } else {
-    document.querySelector(".test").remove("scroll");
-  }
-}); */
-
-/* const checkpoint = 1000;
- 
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll <= checkpoint) {
-    opacity = 1 - currentScroll / checkpoint;
-  } else {
-    opacity = 0;
-  }
-  document.querySelector(".main-overlay").style.opacity = opacity;
-}); */
